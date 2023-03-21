5fcb289bee3a335d2998b86c3d115919c9f72aff
Satyam Atreja <92117949+satyam0111@users.noreply.github.com>
2023-03-21 17:14:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,16 @@ def roll_dice():
 # Create button to roll dice for player 1
 if st.button(f"{player1}, Roll Dice!"):
     roll1 = roll_dice()
-    st.write(f"{player1} rolled a {roll1}")
+st.write(f"{player1} rolled a {roll1}")
 
 # Create button to roll dice for player 2
 if st.button(f"{player2}, Roll Dice!"):
     roll2 = roll_dice()
-    st.write(f"{player2} rolled a {roll2}")
+st.write(f"{player2} rolled a {roll2}")
+
+if roll1 > roll2:
+    st.write(f"{player1} WINS !!")
+else if roll1==roll2:
+    st.write("No one WINS !!")
+else:
+    st.write(f"{player2} WINS !!")
