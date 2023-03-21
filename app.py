02559d6f55de437c87ba4da52cc80216b606dc0e
Satyam Atreja <92117949+satyam0111@users.noreply.github.com>
2023-03-21 17:21:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import random
 import streamlit as st
 
+global_roll1 = 0
+global_roll2 = 0
+
 # Set up player names
 player1 = st.text_input("Enter Player 1 name:")
 player2 = st.text_input("Enter Player 2 name:")
@@ -10,19 +13,17 @@ def roll_dice():
     roll = random.randint(1, 6)
     return roll
 
-# Create button to roll dice for player 1
-if st.button(f"{player1}, Roll Dice!"):
-    roll1 = roll_dice()
-    st.write(f"{player1} rolled a {roll1}")
+def update_rolls():
+    global global_roll1, global_roll2
+    global_roll1 = roll_dice()
+    global_roll2 = roll_dice()
+    
+update_rolls()
 
-# Create button to roll dice for player 2
-if st.button(f"{player2}, Roll Dice!"):
-    roll2 = roll_dice()
-    st.write(f"{player2} rolled a {roll2}")
+st.write(f"{player1} rolled a {global_roll1}")
+st.write(f"{player2} rolled a {global_roll2}")
 
-if roll1 > roll2:
+if global_roll1 > global_roll2:
     st.write(f"{player1} WINS !!")
-elif roll1==roll2:
-    st.write("No one WINS !!")
 else:
     st.write(f"{player2} WINS !!")
